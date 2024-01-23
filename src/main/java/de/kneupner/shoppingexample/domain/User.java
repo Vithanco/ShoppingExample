472dc531bf97449b961b8cbc683faf651a0dda60
Vithanco <klaus@kneupner.de>
2024-01-23 17:16:31 +0100

    in between version

---
+++ src/main/java/de/kneupner/shoppingexample/domain/User.java
@@ -0,0 +1,26 @@
+package de.kneupner.shoppingexample.domain;
+
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+
+
+/*
+* Not integrated due to time constraints
+* would require to add spring-boot-starter-security
+* or (most likely better: spring-security-oauth2-authorization-server)
+* */
+
+//@Data
+//@Entity
+//@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
+//@RequiredArgsConstructor
+//public class User implements UserDetails {
+//    @ID
+//    private final String username;
+//    private final String password;
+//    private final String fullname;
+//    private final String street;
+//    private final String city;
+//}
--- src/test/java/de/kneupner/shoppingexample/ShoppingExampleApplicationTests.java
+++ src/test/java/de/kneupner/shoppingexample/ShoppingExampleApplicationTests.java
@@ -1,13 +1,45 @@
 package de.kneupner.shoppingexample;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ShoppingExampleApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    // should find the correct version from HATEAOS
+    private MediaType halPlusJson = new MediaType("application", "hal+json", Charset.forName("UTF-8"));
+
     @Test
-    void contextLoads() {
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("onlyWebsite"));
     }
 
+    @Test
+    public void testCreateShoppingCart() throws Exception {
+        // Simulate a POST request to create a shopping cart
+        mockMvc.perform(get("/api/products")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        mockMvc.perform(post("/api/CartItems").contentType(MediaType.APPLICATION_JSON)
+                .content("{ product: pro1, count: 3 }")
+                .accept(halPlusJson))
+                .andExpect(status().isCreated());
+    }
 }

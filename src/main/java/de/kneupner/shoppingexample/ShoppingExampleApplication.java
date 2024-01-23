520349e1635c8278b2ee0e22506d270c9a93bc25
Vithanco <klaus@kneupner.de>
2024-01-23 15:58:16 +0100

    Initial commit

---
+++ src/main/java/de/kneupner/shoppingexample/ShoppingExampleApplication.java
@@ -0,0 +1,13 @@
+package de.kneupner.shoppingexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/kneupner/shoppingexample/ShoppingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kneupner.shoppingexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
